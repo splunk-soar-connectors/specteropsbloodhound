d6e92d2f6cd08595d0387f2d1f10c9a54fe4248a
Jacob Davis <jacobd@splunk.com>
2026-07-18 12:21:45 -0700

    chore: refresh connector validation tooling
    Refresh pre-commit hooks to dev-cicd-tools v2.2.4 and record the generated validation metadata.
    
    Written by Codex.

--- specteropsbloodhound_connector.py
+++ specteropsbloodhound_connector.py
@@ -1,6 +1,6 @@
 # File: specteropsbloodhound_consts.py
 #
-# Copyright (c) SpecterOps, 2025
+# Copyright (c) SpecterOps, 2025-2026
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
